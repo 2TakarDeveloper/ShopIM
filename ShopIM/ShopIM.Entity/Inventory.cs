b48f93855b29cc1eabe750afa594ab34d6e30462
tazim <tazimtazim2012@hotmail.com>
2016-09-17 11:52:11 +0600

    1

--- ShopIM/ShopIM.Entity/Inventory.cs
+++ ShopIM/ShopIM.Entity/Inventory.cs
@@ -13,6 +13,7 @@ namespace ShopIM.Entity
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
+        public double SellingPrice { get; set; }
         public DateTime PurchaseDate { get; set; }
 
         public Inventory()
