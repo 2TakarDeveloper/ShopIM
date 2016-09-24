3aca8f515ec9bb6f625d9fd943a316727c76b70c
Shihab <Shihab@DESKTOP-UJENFLB>
2016-09-23 23:51:24 -0700

    log

--- ShopIM/ShopIM.UI/Controller/LOgControl.Designer.cs
+++ ShopIM/ShopIM.UI/Controller/LOgControl.Designer.cs
@@ -30,16 +30,25 @@
         {
             this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
             this.ProfileBox = new System.Windows.Forms.GroupBox();
-            this.AddPictureBtn = new MetroFramework.Controls.MetroButton();
             this.flowPanelLog = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.AddPictureBtn = new MetroFramework.Controls.MetroButton();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.metroPanel1.SuspendLayout();
             this.ProfileBox.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // metroPanel1
             // 
-            this.metroPanel1.Controls.Add(this.ProfileBox);
-            this.metroPanel1.Controls.Add(this.AddPictureBtn);
+            this.metroPanel1.Controls.Add(this.tabControl1);
             this.metroPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.metroPanel1.HorizontalScrollbarBarColor = true;
             this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
@@ -61,32 +70,105 @@
             this.ProfileBox.Controls.Add(this.flowPanelLog);
             this.ProfileBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ProfileBox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ProfileBox.Location = new System.Drawing.Point(15, 13);
+            this.ProfileBox.Location = new System.Drawing.Point(6, 6);
             this.ProfileBox.Name = "ProfileBox";
-            this.ProfileBox.Size = new System.Drawing.Size(715, 405);
+            this.ProfileBox.Size = new System.Drawing.Size(726, 376);
             this.ProfileBox.TabIndex = 11;
             this.ProfileBox.TabStop = false;
-            this.ProfileBox.Text = "Log Details";
-            // 
-            // AddPictureBtn
-            // 
-            this.AddPictureBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.AddPictureBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(210)))), ((int)(((byte)(50)))));
-            this.AddPictureBtn.Location = new System.Drawing.Point(582, 421);
-            this.AddPictureBtn.Name = "AddPictureBtn";
-            this.AddPictureBtn.Size = new System.Drawing.Size(142, 33);
-            this.AddPictureBtn.TabIndex = 15;
-            this.AddPictureBtn.Text = "Clear Log";
-            this.AddPictureBtn.UseCustomBackColor = true;
-            this.AddPictureBtn.UseSelectable = true;
+            this.ProfileBox.Text = "Sales Log List";
             // 
             // flowPanelLog
             // 
             this.flowPanelLog.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowPanelLog.Location = new System.Drawing.Point(3, 18);
             this.flowPanelLog.Name = "flowPanelLog";
-            this.flowPanelLog.Size = new System.Drawing.Size(709, 384);
+            this.flowPanelLog.Size = new System.Drawing.Size(720, 355);
             this.flowPanelLog.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(746, 460);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.AddPictureBtn);
+            this.tabPage1.Controls.Add(this.ProfileBox);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(738, 427);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sales Log";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.metroButton1);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(738, 427);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "User Log";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // AddPictureBtn
+            // 
+            this.AddPictureBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddPictureBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(210)))), ((int)(((byte)(50)))));
+            this.AddPictureBtn.Location = new System.Drawing.Point(590, 388);
+            this.AddPictureBtn.Name = "AddPictureBtn";
+            this.AddPictureBtn.Size = new System.Drawing.Size(142, 33);
+            this.AddPictureBtn.TabIndex = 16;
+            this.AddPictureBtn.Text = "Clear Log";
+            this.AddPictureBtn.UseCustomBackColor = true;
+            this.AddPictureBtn.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(210)))), ((int)(((byte)(50)))));
+            this.metroButton1.Location = new System.Drawing.Point(590, 388);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(142, 33);
+            this.metroButton1.TabIndex = 18;
+            this.metroButton1.Text = "Clear Log";
+            this.metroButton1.UseCustomBackColor = true;
+            this.metroButton1.UseSelectable = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(726, 376);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Log List";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 18);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(720, 355);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // LogControl
             // 
@@ -98,6 +180,10 @@
             this.Size = new System.Drawing.Size(766, 480);
             this.metroPanel1.ResumeLayout(false);
             this.ProfileBox.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -107,6 +193,12 @@
         private MetroFramework.Controls.MetroPanel metroPanel1;
         private System.Windows.Forms.GroupBox ProfileBox;
         private System.Windows.Forms.FlowLayoutPanel flowPanelLog;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private MetroFramework.Controls.MetroButton AddPictureBtn;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
