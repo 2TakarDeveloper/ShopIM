7a1700d043db9ecde3b4b9e1e42e921deb262aeb
Shihab <Shihab@DESKTOP-UJENFLB>
2016-09-26 23:57:48 -0700

    klhk

--- ShopIM/ShopIM.UI/AddProduct.Designer.cs
+++ ShopIM/ShopIM.UI/AddProduct.Designer.cs
@@ -35,8 +35,11 @@
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.metroLink1 = new MetroFramework.Controls.MetroLink();
             this.SubmitButton = new MetroFramework.Controls.MetroButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             ((System.ComponentModel.ISupportInitialize)(this.ProductImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SellingPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // ProductImage
@@ -53,14 +56,14 @@
             // SellingPrice
             // 
             this.SellingPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SellingPrice.Location = new System.Drawing.Point(93, 261);
+            this.SellingPrice.Location = new System.Drawing.Point(108, 257);
             this.SellingPrice.Maximum = new decimal(new int[] {
             9999999,
             0,
             0,
             0});
             this.SellingPrice.Name = "SellingPrice";
-            this.SellingPrice.Size = new System.Drawing.Size(134, 29);
+            this.SellingPrice.Size = new System.Drawing.Size(119, 29);
             this.SellingPrice.TabIndex = 33;
             // 
             // metroLabel8
@@ -97,11 +100,10 @@
             // 
             this.metroLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
             this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Regular;
-            this.metroLabel1.Location = new System.Drawing.Point(97, 232);
+            this.metroLabel1.Location = new System.Drawing.Point(108, 235);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(130, 25);
+            this.metroLabel1.Size = new System.Drawing.Size(100, 19);
             this.metroLabel1.Style = MetroFramework.MetroColorStyle.White;
             this.metroLabel1.TabIndex = 34;
             this.metroLabel1.Text = "Product Name:";
@@ -126,7 +128,7 @@
             this.SubmitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(210)))), ((int)(((byte)(50)))));
             this.SubmitButton.DisplayFocus = true;
             this.SubmitButton.ForeColor = System.Drawing.Color.White;
-            this.SubmitButton.Location = new System.Drawing.Point(140, 296);
+            this.SubmitButton.Location = new System.Drawing.Point(140, 325);
             this.SubmitButton.Margin = new System.Windows.Forms.Padding(0);
             this.SubmitButton.Name = "SubmitButton";
             this.SubmitButton.Size = new System.Drawing.Size(87, 31);
@@ -136,12 +138,42 @@
             this.SubmitButton.UseCustomForeColor = true;
             this.SubmitButton.UseSelectable = true;
             // 
-            // news
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(108, 293);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(119, 29);
+            this.numericUpDown1.TabIndex = 38;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 297);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(84, 25);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.White;
+            this.metroLabel2.TabIndex = 37;
+            this.metroLabel2.Text = "Quantity:";
+            this.metroLabel2.UseCustomBackColor = true;
+            this.metroLabel2.UseStyleColors = true;
+            // 
+            // AddProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(62)))));
-            this.ClientSize = new System.Drawing.Size(270, 336);
+            this.ClientSize = new System.Drawing.Size(270, 377);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.SubmitButton);
             this.Controls.Add(this.metroLink1);
             this.Controls.Add(this.metroLabel1);
@@ -150,10 +182,11 @@
             this.Controls.Add(this.metroLabel3);
             this.Controls.Add(this.ProductImage);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "news";
+            this.Name = "AddProduct";
             this.Text = "news";
             ((System.ComponentModel.ISupportInitialize)(this.ProductImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.SellingPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,5 +201,7 @@
         private MetroFramework.Controls.MetroLabel metroLabel1;
         private MetroFramework.Controls.MetroLink metroLink1;
         private MetroFramework.Controls.MetroButton SubmitButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
     }
 }
